c7083332cbece7060b3b0c9e875dfbbc2a7d3ae2
ProfessorFilipo <filipo.mor@gmail.com>
2025-05-18 19:59:22 -0300

    Create Fornecedor.java

---
+++ TutorialPOO/Fornecedor.java
@@ -0,0 +1,35 @@
+class Fornecedor 
+{
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    // Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    void exibirInfo() {
+        System.out.println("Fornecedor Nome: " + nome + ", Endereço: " + endereco + ", Telefone: " + telefone);
+    }
+}
