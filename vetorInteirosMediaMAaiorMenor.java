f7a5b6875fb9998e336a3cbb8d8763bd0b6c6cba
Ramiro Trejes <ramirotrejes@gmail.com>
2025-09-26 18:38:13 -0300

    alterado array para arraylist

--- vetorInteirosMediaMAaiorMenor.java
+++ vetorInteirosMediaMAaiorMenor.java
@@ -5,34 +5,33 @@ public class Ex02
     {
         Scanner leitor = new Scanner(System.in);
         Random Sorteador = new Random();
-        
-        System.out.printf("\nInforme a quantidade desejada de elementos: ");
-        int quantidade = leitor.nextInt();
-        
-        if(quantidade < 1)
-        {
-            System.out.printf("\nQuantidade informada (%d) é inválida!\n");
-            System.exit(0);
+        ArrayList<Integer> listaNumeros = new ArrayList<>();
+
+        int quantidade;
+        try{
+            System.out.println("Digite quantos números deseja para a média:");
+            quantidade = leitor.nextInt();
+        } catch (Exception e){
+            System.out.println("Valor inválido, tente novamente.");
+            leitor.next();
         }
-        
-        int []numeros = new int[quantidade];
-        
-        int i;
+
         int maior=0, menor=Integer.MAX_VALUE;
         float media = 0.0f;
-        for(i=0; i<quantidade; i++)
+        for(int i = 0; i<quantidade; i++)
         {
-            numeros[i] = Sorteador.nextInt(quantidade*quantidade);
-            if(numeros[i] > maior) { maior = numeros[i]; }
-            if(numeros[i] < menor) { menor = numeros[i]; }
-            media += numeros[i];            
-            System.out.printf("numeros[%2d]: %3d\n", i, numeros[i]); 
+            listaNumeros.add(Sorteador.nextInt(quantidade*quantidade));
+            if(listaNumeros.get(i) > maior) { maior = listaNumeros.get(i); }
+            if(listaNumeros.get(i) < menor) { menor = listaNumeros.get(i); }
+            media += listaNumeros.get(i);
+            System.out.printf("numeros[%2d]: %3d\n", i, listaNumeros.get(i));
         }
-        
+
         System.out.printf("\nMaior valor: %d\nMenor valor: %d\nMedia: %2.2f\n", maior, menor, media/quantidade);
-        
-        
-        
-        
+
+
+
+
     }
 }
+
